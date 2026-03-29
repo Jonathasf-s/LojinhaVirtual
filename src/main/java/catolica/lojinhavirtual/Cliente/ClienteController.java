88b6464ca126143d838a4b61b23e2da1c982d97b
Jonathas <funguinha82@gmail.com>
2026-03-29 19:15:37 -0300

    feat: ProdutosController finalizado :: updade: ClienteController mapping padronizado

--- src/main/java/catolica/lojinhavirtual/Cliente/ClienteController.java
+++ src/main/java/catolica/lojinhavirtual/Cliente/ClienteController.java
@@ -16,23 +16,23 @@ private ClienteService clienteService;
     }
 
     //Adicionar cliente
-    @PostMapping("/novoUser")
+    @PostMapping("/add")
     public ClienteModel NovoUsuario( @RequestBody ClienteModel cliente){
         return clienteService.criarCliente(cliente);
     }
 
     //Listar Clientes
-    @GetMapping("/mostrarClientes")
+    @GetMapping("/listar")
     public List<ClienteModel>MostrarTodosClientes(){
         return clienteService.listarClientes();
     }
 
     //listar cliente por ID
-    @GetMapping("/mostrarClientes/{id}")
+    @GetMapping("/Id/{id}")
     public ClienteModel mostrarClientesId(@PathVariable Long id){return clienteService.listarClientesPorId(id);}
 
     //deletar cliente
-    @DeleteMapping("/deletarCliente/{id}")
+    @DeleteMapping("/delete/{id}")
     public String deletarCliente(@PathVariable Long id){
         return "Cliente com ID" + id + "deletado";
     }
--- src/main/java/catolica/lojinhavirtual/Produtos/ProdutosController.java
+++ src/main/java/catolica/lojinhavirtual/Produtos/ProdutosController.java
@@ -1,7 +1,8 @@
 package catolica.lojinhavirtual.Produtos;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("produto")
@@ -13,4 +14,27 @@ public class ProdutosController {
         this.produtosService = produtosService;
     }
 
+    // adicionar novo produto
+    @PostMapping("/add")
+    public ProdutosModel adicionar(@RequestBody ProdutosModel produto) {
+        return produtosService.adicionarProduto(produto);
+    }
+
+    // Listar todos
+    @GetMapping("/listar")
+    public List<ProdutosModel> listarTodos() {
+        return produtosService.listarProduto();
+    }
+
+    //Mostar por Id
+    @GetMapping("/Id/{id}")
+    public ProdutosModel buscarPorId(@PathVariable Long id) {
+        return produtosService.produtosPorID(id);
+    }
+    @DeleteMapping("/deletar/{id}")
+    public void remover(@PathVariable Long id) {
+        produtosService.DeletarProduto(id);
+    }
+
 }
+
