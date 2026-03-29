4ab3fe160b102db487dbba7b7749231ad5a9e62b
Jonathas <funguinha82@gmail.com>
2026-03-29 16:34:52 -0300

    fix: tipo de geraçao, mudou de AUTO para IDENTITY

--- src/main/java/catolica/lojinhavirtual/Cliente/ClienteController.java
+++ src/main/java/catolica/lojinhavirtual/Cliente/ClienteController.java
@@ -1,10 +1,28 @@
 package catolica.lojinhavirtual.Cliente;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("cliente")
 
 public class ClienteController {
+
+private ClienteService clienteService;
+
+    public ClienteController(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    @PostMapping("/novoUser")
+    public String NovoUsuario(){return "crie um novo usuario";}
+
+    @GetMapping("/mostrarClientes")
+    public List<ClienteModel>MostrarTodosClientes(){
+        return clienteService.listarClientes();
+    }
 }
