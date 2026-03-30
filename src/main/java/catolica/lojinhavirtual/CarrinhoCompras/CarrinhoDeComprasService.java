561d7476e675c1f2969a497f6df77b695af4a2e9
Jonathas <funguinha82@gmail.com>
2026-03-30 16:55:18 -0300

    Feat: CarrinhoDeComprasService finalizado

--- src/main/java/catolica/lojinhavirtual/CarrinhoCompras/CarrinhoDeComprasService.java
+++ src/main/java/catolica/lojinhavirtual/CarrinhoCompras/CarrinhoDeComprasService.java
@@ -1,4 +1,47 @@
 package catolica.lojinhavirtual.CarrinhoCompras;
 
+import catolica.lojinhavirtual.Produtos.ProdutosModel;
+import catolica.lojinhavirtual.Produtos.ProdutosRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CarrinhoDeComprasService {
+
+    private CarrinhoDeComprasRepository carrinhoDeComprasRepository;
+
+   private ProdutosRepository produtosRepository;
+
+    public CarrinhoDeComprasService(ProdutosRepository produtosRepository, CarrinhoDeComprasRepository carrinhoDeComprasRepository) {
+        this.produtosRepository = produtosRepository;
+        this.carrinhoDeComprasRepository = carrinhoDeComprasRepository;
+    }
+
+    // Salvar no carrinho
+    public CarrinhoDeComprasModel finalizarCompra(CarrinhoDeComprasModel carrinho){
+        return carrinhoDeComprasRepository.save(carrinho);
+    }
+
+    //Soma dos produtos
+    public double calcularTotal(Long carrinhoId){
+        CarrinhoDeComprasModel carrinho = carrinhoDeComprasRepository.findById(carrinhoId).orElse(null);
+        if (carrinho != null && carrinho.getProdutos() !=null){
+            return carrinho.getProdutos().stream()
+                    .mapToDouble(ProdutosModel::getProdutosValor)
+                    .sum();
+        }
+        return 0.0;
+    }
+
+    //Listar os o conteudo do carrinho
+    public List<CarrinhoDeComprasModel> listarTudo(){
+        return carrinhoDeComprasRepository.findAll();
+    }
+
+    // Buscar por ID
+    public CarrinhoDeComprasModel buscarId(Long id){
+        return carrinhoDeComprasRepository.findById(id).orElse(null);
+    }
+
 }
